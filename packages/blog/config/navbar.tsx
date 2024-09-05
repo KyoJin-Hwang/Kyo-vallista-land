0bbfc1d8d833abce6debefd587cbe5152eff67da
owen <owen1127@naver.com>
2024-09-05 16:40:09 +0900

    fix : facebook 제거 및 velog - facebook 사용 하지 않으므로 velog 로 이동

--- packages/blog/config/navbar.tsx
+++ packages/blog/config/navbar.tsx
@@ -88,7 +88,7 @@ export const NavFooter: NavFooterType = {
         <path d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22' />
       </svg>
     ),
-    link: 'https://github.com/vallista'
+    link: 'https://github.com/Kyojin-Hwang'
   },
   twitter: {
     name: '',
@@ -110,22 +110,17 @@ export const NavFooter: NavFooterType = {
     link: ''
   },
   secondary: {
-    name: 'Facebook',
+    name: 'velog',
     icon: (
-      <svg
-        viewBox='0 0 24 24'
-        width='32'
-        height='32'
-        stroke='currentColor'
-        strokeWidth='1.5'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        fill='none'
-        shapeRendering='geometricPrecision'
-      >
-        <path d='M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z' />
+      <svg width='192' height='192' viewBox='0 0 192 192' fill='currentColor'>
+        <path
+          fill-rule='evenodd'
+          clip-rule='evenodd'
+          d='M24 0H168C181.255 0 192 10.7451 192 24V168C192 181.255 181.255 192 168 192H24C10.7451 192 0 181.255 0 168V24C0 10.7451 10.7451 0 24 0ZM49 57.9199V65.48H67L80.6799 142.52L98.5 141.26C116.02 119.06 127.84 102.44 133.96 91.3999C140.2 80.24 143.32 70.9399 143.32 63.5C143.32 59.0601 142 55.7 139.36 53.4199C136.84 51.1399 133.66 50 129.82 50C122.62 50 116.62 53.0601 111.82 59.1799C116.5 62.3 119.68 64.8799 121.36 66.9199C123.16 68.8401 124.06 71.4199 124.06 74.6599C124.06 80.0601 122.44 86.1799 119.2 93.02C116.08 99.8601 112.66 105.92 108.94 111.2C106.54 114.56 103.48 118.7 99.76 123.62L88.0601 57.2C87.1001 52.3999 84.1001 50 79.0601 50C76.78 50 72.3999 50.96 65.9199 52.8799C59.4399 54.6799 53.8 56.3601 49 57.9199Z'
+          fill='currentColor'
+        ></path>
       </svg>
     ),
-    link: 'https://facebook.com/gwangwhi.ma'
+    link: 'https://velog.io/@owen1127/posts'
   }
 }
