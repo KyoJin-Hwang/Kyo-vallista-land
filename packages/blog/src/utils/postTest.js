2735ba03f165b843cf1cb18c4128e39a28a86190
owen <owen1127@naver.com>
2024-11-14 18:36:18 +0900

    post : js interview 정리중

--- packages/blog/src/utils/postTest.js
+++ packages/blog/src/utils/postTest.js
@@ -1,36 +1,17 @@
-// 함수를 인수로 전달받고 함수를 반환하는 고차 함수
-// 이 함수는 카운트 상태를 유지하기 위한 자유 변수 counter를 기억하는 클로저를 반환한다.
-
-function makeCounter(aux) {
-  // 카운트 상태를 유지하기 위한 자유 변수
-  let counter = 0
-
-  // 클로저를 반환
-  return function () {
-    // 인수로 전달받은 보조 함수에 상태 변경을 위임한다.
-    counter = aux(counter)
-    return counter
-  }
+function greet(name) {
+  console.log(`Hello, ${name}! I am ${this.name}.`)
 }
 
-// 보조 함수
-function increase(n) {
-  return ++n
+const person = { name: 'Alice' }
+
+// `this`를 `person` 객체로 지정하고, 첫 번째 인수를 'Bob'으로 전달
+greet.call(person, 'Bob') // 출력: Hello, Bob! I am Alice.
+
+function greet2(name, age) {
+  console.log(`Hello, ${name}! I am ${this.name}, and I am ${age} years old.`)
 }
 
-// 보조 함수
-function decrease(n) {
-  return --n
-}
+const person2 = { name: 'Alice' }
 
-// 함수로 함수를 생성한다.
-// makeCounter 함수는 보조 함수를 인수로 전달받아 함수를 반환한다.
-
-const increaser = makeCounter(increase)
-console.log(increaser()) // 1
-console.log(increaser()) // 2
-
-// increaser 함수와는 별개의 독립된 렉시컬 환경을 갖기 때문에 카운터 상태가 연동하지 않는다.
-const decreaser = makeCounter(decrease)
-console.log(decreaser()) // -1
-console.log(decreaser()) // -2
+// `this`를 `person` 객체로 지정하고, 인수들을 배열로 전달
+greet2.apply(person, ['Bob', 30]) // 출력: Hello, Bob! I am Alice, and I am 30 years old.
